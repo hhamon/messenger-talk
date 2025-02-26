898b7b6a22d1a9be1e5bf47635debc2fffc706e4
Hugo Hamon <hhamon@apple.com>
2025-02-26 06:41:55 -0500

    Add functional tests with `zenstruck/messenger-test` library

--- tests/Controller/RegisterUserControllerTest.php
+++ tests/Controller/RegisterUserControllerTest.php
@@ -4,40 +4,56 @@ declare(strict_types=1);
 
 namespace App\Tests\Controller;
 
+use App\Entity\User;
+use App\Repository\DoctrineUserRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\Messenger\Transport\InMemory\InMemoryTransport;
+use Zenstruck\Browser\Test\HasBrowser;
+use Zenstruck\Foundry\Test\ResetDatabase;
+use Zenstruck\Mailer\Test\InteractsWithMailer;
+use Zenstruck\Messenger\Test\InteractsWithMessenger;
 
 final class RegisterUserControllerTest extends WebTestCase
 {
+    use HasBrowser;
+    use InteractsWithMailer;
+    use InteractsWithMessenger;
+    use ResetDatabase;
+
     public function testRegisterUser(): void
     {
-        $client = static::createClient();
-        $client->request('GET', '/registration');
+        $this->transport('async_priority_high')->queue()->assertEmpty();
+        $this->transport('async_priority_low')->queue()->assertEmpty();
 
-        self::assertResponseIsSuccessful();
+        $this->browser()
+            ->followRedirects()
+            ->visit('/registration')
+            ->fillField('user_registration[gender]', 'male')
+            ->fillField('user_registration[fullName]', 'John Smith')
+            ->fillField('user_registration[birthdate]', '1978-02-26')
+            ->fillField('user_registration[country]', 'US')
+            ->fillField('user_registration[email]', 'jmsmith@example.com')
+            ->fillField('user_registration[password][first]', 'jt/%Xq}EW"8`5?wmVxhN&;')
+            ->fillField('user_registration[password][second]', 'jt/%Xq}EW"8`5?wmVxhN&;')
+            ->click('Submit')
+            ->assertOn('/registration/confirmation')
+            ->assertSeeIn('h1', 'Signup Confirmation')
+            ->assertSeeIn('.feedback', 'Welcome! Your registration is complete.');
 
-        $client->submitForm('Submit', [
-            'user_registration[gender]' => 'male',
-            'user_registration[fullName]' => 'John Smith',
-            'user_registration[birthdate]' => '1978-02-26',
-            'user_registration[country]' => 'US',
-            'user_registration[email]' => 'jmsmith@example.com',
-            'user_registration[password][first]' => 'jt/%Xq}EW"8`5?wmVxhN&;',
-            'user_registration[password][second]' => 'jt/%Xq}EW"8`5?wmVxhN&;',
-        ]);
+        $this->transport('async_priority_high')->queue()->assertCount(1);
+        $this->transport('async_priority_high')->processOrFail();
 
-        /** @var InMemoryTransport $transport */
-        $transport = self::getContainer()->get('messenger.transport.async_priority_high');
-        $this->assertCount(1, $transport->getSent());
+        $this->transport('async_priority_low')->queue()->assertCount(1);
+        $this->transport('async_priority_low')->processOrFail();
 
-        /** @var InMemoryTransport $transport */
-        $transport = self::getContainer()->get('messenger.transport.async_priority_low');
-        $this->assertCount(1, $transport->getSent());
+        $this->mailer()
+            ->assertSentEmailCount(1)
+            ->assertEmailSentTo('jmsmith@example.com', 'Please Confirm your Email');
 
-        $client->followRedirect();
+        /** @var User $user */
+        $user = self::getContainer()->get(UserRepository::class)->findOneBy(['email' => 'jmsmith@example.com']);
 
-        self::assertResponseIsSuccessful();
-        self::assertSelectorTextSame('h1', 'Signup Confirmation');
-        self::assertSelectorTextSame('.feedback', 'Welcome! Your registration is complete.');
+        self::assertSame(md5('jmsmith@example.com'), $user->getIntercomId());
     }
 }
